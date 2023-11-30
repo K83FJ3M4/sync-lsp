87332c73e4a8ce3e0bddc2370e8eb87ac9815df2
K83FJ3M4 <phillip.mendel@posteo.de>
2023-11-30 18:48:06 +0100

    added definition documentation

--- sync-lsp/src/text_document/definition.rs
+++ sync-lsp/src/text_document/definition.rs
@@ -1,3 +1,9 @@
+//! implementation of the `textDocument/definition` request
+//! 
+//! # Usage
+//! [`Server::on_definition`] is invoked, to compute the definition
+//! of a symbol at a given cursor position.
+
 use crate::TypeProvider;
 use crate::{Server, connection::Endpoint};
 use crate::connection::Callback;
@@ -16,6 +22,16 @@ impl DefinitionOptions {
 }
 
 impl<T: TypeProvider> Server<T> {
+
+    /// Sets the callback that will be called to locate a  [definition](self).
+    /// 
+    /// # Argument
+    /// * `callback` - A callback which is called with the following parameters to resolve a definition:
+    ///     * The server instance receiving the response.
+    ///     * The [`TextDocumentIdentifer`] of the document for which a definition is requested.
+    ///    * The [`Position`] at which a definition is requested.
+    ///     * `return` - A list of [`Location`]s to display.
+
     pub fn on_definition(&mut self, callback: fn(&mut Server<T>, TextDocumentIdentifer, Position) -> Vec<Location>) {
         self.text_document.definition.set_callback(Callback::request(move |server, params: TextDocumentPositionParams | {
             callback(server, params.text_document, params.position)
