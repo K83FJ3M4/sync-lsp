f300103d3cd25368352ea80281ae30b2aef85d46
Phillip Mendel <135428948+K83FJ3M4@users.noreply.github.com>
2023-12-04 09:50:40 +0000

    added documentation for symbols and formatting

--- sync-lsp/src/text_document/document_symbol.rs
+++ sync-lsp/src/text_document/document_symbol.rs
@@ -1,3 +1,9 @@
+//! implementation of the `textDocument/documentSymbol` request
+//! 
+//! # Usage
+//! The client can request a list of symbols from the server via [`Server::on_document_symbol`]
+//! at any time. This is useful for the user to navigate to a specific symbol in a file.
+
 use crate::TypeProvider;
 use crate::workspace::symbol::SymbolInformation;
 use crate::{Server, connection::Endpoint};
@@ -24,6 +30,15 @@ impl DocumentSymbolOptions {
 }
 
 impl<T: TypeProvider> Server<T> {
+
+    /// Sets the callback that will be called to [compute document symbols](self).
+    /// 
+    /// # Argument
+    /// * `callback` - A callback which is called with the following parameters as soon as document symbols are requested:
+    ///     * The server instance receiving the response.
+    ///     * The [`TextDocumentIdentifer`] of the document that has been opened.
+    ///     * `return` - A list of symbols to display.
+
     pub fn on_document_symbol(&mut self, callback: fn(&mut Server<T>, TextDocumentIdentifer) -> Vec<SymbolInformation>) {
         self.text_document.document_symbol.set_callback(Callback::request(move |server, params: DocumentSymbolParams| {
             callback(server, params.text_document)
--- sync-lsp/src/text_document/formatting.rs
+++ sync-lsp/src/text_document/formatting.rs
@@ -1,3 +1,9 @@
+//! implementation of the `textDocument/formatting` request
+//! 
+//! # Usage
+//! Some server may be capable of formatting a document. If so, [`Server::on_formatting`] can be used to
+//! provide this functionality to the client. It can be triggered either manually or automatically.
+
 use crate::TypeProvider;
 use crate::{Server, connection::Endpoint};
 use crate::connection::Callback;
@@ -14,11 +20,15 @@ struct DocumentFormattingParams {
     options: FormattingOptions
 }
 
+/// Specifies how a document should be formatted.
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct FormattingOptions<T = ()> {
+    /// The size of tabs in spaces.
     pub tab_size: u32,
+    /// Specifies whether tabs should be used instead of spaces.
     pub insert_spaces: bool,
+    /// Additional options can be handled by the server if a struct with the corresponding fields is provided here.
     #[serde(flatten)]
     pub properties: T
 }
@@ -33,6 +43,16 @@ impl DocumentFormattingOptions {
 }
 
 impl<T: TypeProvider> Server<T> {
+
+    /// Sets the callback that will be called to [format a document](self).
+    ///
+    /// # Argument
+    /// * `callback` - A callback which is called with the following parameters as soon as a document is formatted:
+    ///     * `server` - The server on which the request was received.
+    ///     * `document` - The [`TextDocumentIdentifer`] of the document that has been opened.
+    ///     * `options` - The [`FormattingOptions`] that specify how the document should be formatted.
+    ///     * `return` - A list of edits to apply to the document.
+
     pub fn on_formatting(&mut self, callback: fn(&mut Server<T>, TextDocumentIdentifer, FormattingOptions) -> Vec<TextEdit>) {
         self.text_document.formatting.set_callback(Callback::request(move |server, params: DocumentFormattingParams | {
             callback(server, params.text_document, params.options)
