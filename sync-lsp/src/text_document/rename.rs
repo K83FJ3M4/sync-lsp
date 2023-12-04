1c0c1240fa0b899f3b38d07e11256e8693a0ffd4
Phillip Mendel <135428948+K83FJ3M4@users.noreply.github.com>
2023-12-04 10:31:24 +0000

    added rename and will-save-wait-until documentation

--- sync-lsp/src/text_document/rename.rs
+++ sync-lsp/src/text_document/rename.rs
@@ -1,3 +1,9 @@
+//! implementation of the `textDocument/rename` request
+//! 
+//! # Usage
+//! If a user chooses to rename a symbol, the server can be notified via [`Server::on_rename`]
+//! and also rename all references to that symbol.
+
 use crate::TypeProvider;
 use crate::workspace::apply_edit::WorkspaceEdit;
 use crate::{Server, connection::Endpoint};
@@ -25,6 +31,17 @@ impl RenameOptions {
 }
 
 impl<T: TypeProvider> Server<T> {
+
+    /// Sets the callback that will be called to [rename symbols](self).
+    /// 
+    /// # Argument
+    /// * `callback` - A callback which is called with the following parameters as soon as a symbol is renamed:
+    ///     * The server instance receiving the response.
+    ///     * The [`TextDocumentIdentifer`] of the target document.
+    ///     * The [`Position`] of the cursor.
+    ///     * The new name of the symbol.
+    ///     * `return` - A [`WorkspaceEdit`] that contains the changes to apply to the workspace.
+
     pub fn on_rename(&mut self, callback: fn(&mut Server<T>, TextDocumentIdentifer, Position, String) -> WorkspaceEdit) {
         self.text_document.rename.set_callback(Callback::request(move |server, params: RenameParams | {
             callback(server, params.text_document, params.position, params.new_name)
--- sync-lsp/src/text_document/signature_help.rs
+++ sync-lsp/src/text_document/signature_help.rs
@@ -1,3 +1,10 @@
+//! implementation of the `textDocument/signatureHelp` request
+//! 
+//! # Usage
+//! A client can request the signature of a item using [`Server::on_signature_help`].
+//! Additionally, the specific characters that trigger signature help can be set via
+//! [`Server::set_signature_help_trigger_characters`].
+
 use crate::TypeProvider;
 use crate::{Server, connection::Endpoint};
 use crate::connection::Callback;
@@ -10,24 +17,35 @@ pub(crate) struct SignatureHelpOptions {
     trigger_characters: Vec<String>
 }
 
+/// The signature of some item.
 #[derive(Serialize, Debug, Default)]
 pub struct SignatureHelp {
+    /// One or more signatures.
     pub signatures: Vec<SignatureInformation>,
+    /// The index of the active signature.
     pub active_signature: Option<u32>,
+    /// The index of the active parameter.
     pub active_parameter: Option<u32>
 }
 
+/// Represents the signature of some item.
 #[derive(Serialize, Debug)]
 pub struct SignatureInformation {
+    /// A string representing the name of the item.
     pub label: String,
+    /// Documentation of the item.
     pub documentation: Option<String>,
+    /// The parameters of this signature.
     #[serde(skip_serializing_if = "Vec::is_empty")]
     pub parameters: Vec<ParameterInformation>
 }
 
+/// Represents a parameter of a callable-signature.
 #[derive(Serialize, Debug)]
 pub struct ParameterInformation {
+    /// The label of this parameter.
     pub label: String,
+    /// The markdown documentation of this parameter.
     pub documentation: Option<String>
 }
 
@@ -41,11 +59,26 @@ impl SignatureHelpOptions {
 }
 
 impl<T: TypeProvider> Server<T> {
+
+    /// Sets the callback that will be called to [compute signature help](self).
+    /// 
+    /// # Argument
+    /// * `callback` - A callback which is called with the following parameters as soon as signature help is requested:
+    ///     * The server instance receiving the response.
+    ///     * The [`TextDocumentIdentifer`] of the target document.
+    ///     * The [`Position`] of the cursor.
+    ///     * `return` - The signature help to display.
+
     pub fn on_signature_help(&mut self, callback: fn(&mut Server<T>, TextDocumentIdentifer, Position) -> SignatureHelp) {
         self.text_document.signature_help.set_callback(Callback::request(move |server, params: TextDocumentPositionParams| {
             callback(server, params.text_document, params.position)
         }))
     }
+
+    /// Sets the characters that trigger [signature help](self).
+    /// 
+    /// # Argument
+    /// * `value` - The characters that trigger signature help.
 
     pub fn set_signature_help_trigger_characters(&mut self, value: Vec<String>) {
         self.text_document.signature_help.options_mut().trigger_characters = value;
